644bab1dba4eea3929392e698b3fc821d0e7f547
minju <sw.deliminju@gmail.com>
2026-03-23 12:10:04 +0900

    chore: .gitignore 최신화 및 초기 환경 설정 완료

---
+++ src/test/java/com/gachi/be/GachiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachi.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
