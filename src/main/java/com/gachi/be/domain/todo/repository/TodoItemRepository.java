4e98f376edb6ee9341e888a3aca97bbdc531e59a
minkyung <HaMming.bright@gmail.com>
2026-04-13 19:48:59 +0900

    feat: 레포지토리 로직 추가 및 프롬프팅 추가

---
+++ src/main/java/com/gachi/be/domain/todo/repository/TodoItemRepository.java
@@ -0,0 +1,14 @@
+package com.gachi.be.domain.todo.repository;
+
+import com.gachi.be.domain.todo.entity.TodoItem;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+
+    /** 특정 가정통신문의 해야 할 일 항목 전체 조회 (생성 순서대로) */
+    List<TodoItem> findByNewsletterIdOrderByIdAsc(Long newsletterId);
+
+    /** 특정 가정통신문의 해야 할 일 항목 전체 삭제 (재분석 시 사용) */
+    void deleteByNewsletterId(Long newsletterId);
+}
