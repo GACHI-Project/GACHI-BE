8a6296260fc41b15645693ce95c4ad8de4797327
minkyung <HaMming.bright@gmail.com>
2026-05-10 19:15:44 +0900

    feat: 체크리스트 완료 처리 dto

---
+++ src/main/java/com/gachi/be/domain/checklist/dto/request/ChecklistCompleteRequest.java
@@ -0,0 +1,9 @@
+package com.gachi.be.domain.checklist.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ChecklistCompleteRequest(
+
+    @NotNull(message = "isCompleted는 필수입니다.")
+    Boolean isCompleted
+) {}
