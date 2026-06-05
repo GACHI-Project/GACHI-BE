7b9f2ea9fbb11115c22bb75857a57c4c9376b707
minkyung <HaMming.bright@gmail.com>
2026-06-06 01:15:59 +0900

    feat: 서비스 파일 추가

---
+++ src/main/java/com/gachi/be/domain/schoolguide/service/SchoolGuideService.java
@@ -0,0 +1,33 @@
+package com.gachi.be.domain.schoolguide.service;
+
+import com.gachi.be.domain.schoolguide.dto.request.SchoolGuideCreateRequest;
+import com.gachi.be.domain.schoolguide.dto.request.SchoolGuideUpdateRequest;
+import com.gachi.be.domain.schoolguide.dto.response.SchoolGuideCategoryResponse;
+import com.gachi.be.domain.schoolguide.dto.response.SchoolGuideDetailResponse;
+import com.gachi.be.domain.schoolguide.dto.response.SchoolGuideListResponse;
+import com.gachi.be.domain.schoolguide.dto.response.SchoolGuidePopularResponse;
+import com.gachi.be.domain.schoolguide.entity.enums.SchoolGuideCategory;
+
+public interface SchoolGuideService {
+
+    /** 카테고리별 FAQ 개수 조회 */
+    SchoolGuideCategoryResponse getCategoryCounts();
+
+    /** 주간 인기 질문 TOP 2 */
+    SchoolGuidePopularResponse getPopularFaqs();
+
+    /** FAQ 목록 조회 */
+    SchoolGuideListResponse getFaqs(SchoolGuideCategory category, String search);
+
+    /** FAQ 상세 조회 + weekly_view_count 증가 */
+    SchoolGuideDetailResponse getFaqDetail(Long faqId);
+
+    /** FAQ 등록*/
+    Long createFaq(SchoolGuideCreateRequest request);
+
+    /** FAQ 수정 */
+    void updateFaq(Long faqId, SchoolGuideUpdateRequest request);
+
+    /** FAQ 삭제 */
+    void deleteFaq(Long faqId);
+}
