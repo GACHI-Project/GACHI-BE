53c82338f9f7ef83b60fbb1c0ac4de042998b136
minkyung <HaMming.bright@gmail.com>
2026-06-06 01:09:57 +0900

    feat: 카테고리 응답 response dto 추가

---
+++ src/main/java/com/gachi/be/domain/schoolguide/dto/response/SchoolGuideCategoryResponse.java
@@ -0,0 +1,28 @@
+package com.gachi.be.domain.schoolguide.dto.response;
+
+import com.gachi.be.domain.schoolguide.entity.enums.SchoolGuideCategory;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SchoolGuideCategoryResponse {
+
+    private List<CategoryItem> categories;
+
+    public static SchoolGuideCategoryResponse of(List<CategoryItem> categories) {
+        return SchoolGuideCategoryResponse.builder().categories(categories).build();
+    }
+
+    @Getter
+    @Builder
+    public static class CategoryItem {
+        private SchoolGuideCategory category;
+        private long count;
+
+        public static CategoryItem of(SchoolGuideCategory category, long count) {
+            return CategoryItem.builder().category(category).count(count).build();
+        }
+    }
+}
