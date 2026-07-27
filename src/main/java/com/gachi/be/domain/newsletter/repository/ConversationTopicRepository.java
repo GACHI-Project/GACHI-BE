d3a386faf3eea9fc89a914772a076c4fc420b257
minkyung <HaMming.bright@gmail.com>
2026-07-27 18:22:11 +0900

    feat: spotless 반영

--- src/main/java/com/gachi/be/domain/newsletter/repository/ConversationTopicRepository.java
+++ src/main/java/com/gachi/be/domain/newsletter/repository/ConversationTopicRepository.java
@@ -8,6 +8,6 @@ public interface ConversationTopicRepository extends JpaRepository<ConversationT
 
   List<ConversationTopic> findAllByNewsletterIdOrderByIdAsc(Long newsletterId);
 
-    /** 특정 가정통신문의 대화 주제 전체 삭제 (재분석 시 사용). */
-    void deleteByNewsletterId(Long newsletterId);
+  /** 특정 가정통신문의 대화 주제 전체 삭제 (재분석 시 사용). */
+  void deleteByNewsletterId(Long newsletterId);
 }
